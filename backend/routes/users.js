6f832f77156f94e53df824d713da377dbbb850ce
dpandev <davidpanesiu@gmail.com>
2022-01-01 17:32:07 -0700

    updated routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -44,14 +44,37 @@ router.delete("/:id", async (req, res) => { //delete process
 })
 
 //get a user
-router.get('/:id', async (req, res) => {
+router.get('/', async (req, res) => {
+  const userId = req.query.userId;
+  const username = req.query.username;
   try {
-    const user = await User.findById(req.params.id)
-    //limit data sent back from server (dont show password, etc.)
-    const {password, updatedAt, ...other} = user._doc
-    res.status(200).json(other) //sends info with exclusions
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username: username });
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+//get friends
+router.get("/friends/:userId", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId)
+    const friends = await Promise.all(
+      user.followings.map(friendId => {
+        return User.findById(friendId)
+      })
+    )
+    let friendList = []
+    friends.map((friend) => { //grabs specific properties for displaying friends list
+      const { _id, username, profilePicture } = friend
+      friendList.push({ _id, username, profilePicture })
+    })
+    res.status(200).json(friendList)
   } catch(err) {
-    return res.status(500).json(err)
+    res.status(500).json(err)
   }
 })
 
